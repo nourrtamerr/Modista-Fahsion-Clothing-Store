3f488f3d4fcd8c9c5b559107312102ce988ea987
Malak Emad <119331267+malakemadd@users.noreply.github.com>
2025-04-22 16:06:33 +0200

    Update nav-bar.component.ts

--- src/app/Components/nav-bar/nav-bar.component.ts
+++ src/app/Components/nav-bar/nav-bar.component.ts
@@ -1,42 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { Router } from '@angular/router';
 import { UserAuthService } from '../../Services/User/user-auth.service';
 import { isAuthenticated } from '../../Models/user/user';
 
-
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink,RouterLinkActive],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent implements OnInit {
-  islogged!:isAuthenticated| undefined;
-  error: string|null=null;
-  constructor(private userAuthService: UserAuthService,private router:Router) {
-    
-  }
+  islogged: isAuthenticated = { isAuthenticated: false, userName: '' };
+
+  constructor(private userAuthService: UserAuthService, private router: Router) {}
+
   ngOnInit(): void {
-    this.userAuthService.getUserLogged().subscribe({
-      next: (data)=>{this.islogged=data,
-        console.log(this.islogged);
-      },
-      error: (err)=> this.error=err
-    }
-    )
-    console.log(this.islogged?.isAuthenticated,this.islogged?.userName);
-    console.log("hiii");
+    this.userAuthService.authStatus$.subscribe({
+      next: (data) => this.islogged = data
+    });
+
+    this.userAuthService.refreshAuthStatus();
   }
-  logout(){
+
+  logout() {
     this.userAuthService.logout().subscribe({
-      next: (str) => {
-        this.islogged!.isAuthenticated=false,
-        console.log('form submitted',str);
-        if ( str.status=== 200) {
+      next: (res) => {
+        if (res.status === 200) {
           this.router.navigate(['/login']);
         }
-      }
-  })
+      },
+      error: (err) => console.error('Logout failed', err)
+    });
   }
 }
-
