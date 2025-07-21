47dcc3aa1059d01447cb811d3e3d6d4d6fe370e9
nourrtamerr <nourtamer202@gmail.com>
2025-07-21 05:06:20 +0300

    adminEdits4

--- src/app/Components/AdminPages/admin-sub-categories/admin-sub-categories.component.ts
+++ src/app/Components/AdminPages/admin-sub-categories/admin-sub-categories.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../../Services/products.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { category } from '../../../Models/product';
 
 @Component({
   selector: 'app-admin-sub-categories',
@@ -14,12 +15,24 @@ export class AdminSubCategoriesComponent implements OnInit {
   constructor(private productService:ProductsService){}
 
   subcategories:any
+  categoryMap: { [key: number]: string } = {};
 
   ngOnInit(): void {
     this.productService.getSubCategories().subscribe({
       next:(data)=>{this.subcategories=data; console.log(data)},
       error:(err)=>console.log(err)
     })
+
+    this.productService.getCategories().subscribe((cats: category[]) => {
+      this.categoryMap = cats.reduce((acc, cat) => {
+        acc[cat.id] = cat.name;
+        return acc;
+      }, {} as { [key: number]: string });
+    });
+  }
+
+  getCategoryName(id: number): string {
+    return this.categoryMap[id] || 'Unknown';
   }
 
 }
